196583a75e174633bae76b7045b0f9fec662493c
Gambinha <fernandogamba19@gmail.com>
2024-05-14 23:34:56 -0300

    feat: adding shadui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return <h1>Constructor Modelo</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-700 text-center">
+        Hello world!
+      </h1>
+      <Button className="w-48">Entrar</Button>
+    </div>
+  );
 }
 
 export default App;
